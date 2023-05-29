3c390745fbf00a6ed4b89b53023ac081b5050de6
Abhijeet Kumar <abhijeet.kumar@fnf.com>
2023-05-30 02:28:56 +0530

    Add project files.

---
+++ Controllers/CheckoutController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SupermarketCheckout.Services;
+using System;
+
+namespace SupermarketCheckout.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CheckoutController : ControllerBase
+    {
+        private readonly CheckoutServices _checkout;
+
+        public CheckoutController(CheckoutServices checkout)
+        {
+            _checkout = checkout;
+        }
+
+        [HttpGet("{skus}")]
+        public IActionResult Checkout(string skus)
+        {
+            try
+            {
+                int totalPrice = _checkout.Checkout(skus);
+                return Ok(totalPrice);
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ Tests/CheckoutService.cs
@@ -0,0 +1,21 @@
+﻿using SupermarketCheckout.Models;
+
+namespace SupermarketCheckout.Tests
+{
+    internal class CheckoutService
+    {
+        public CheckoutService()
+        {
+        }
+
+        internal void AddItem(Item item)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal object Checkout(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
